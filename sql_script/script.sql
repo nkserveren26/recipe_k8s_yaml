46fff49b7e85bf7d9f957865ea37c8b3f047fb14
nkserveren26 <norinoritoshi@outlook.jp>
2025-06-29 10:07:50 +0900

    アプリ用DBユーザーを作成。#12

--- sql_script/script.sql
+++ sql_script/script.sql
@@ -6,3 +6,15 @@ GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, INDEX, REFERENCES ON 
 
 /* 開発環境のMySQLデータのダンプファイルを作成 */
 mysqldump -u root -p recipe > recipe.sql
+
+/* インポート先環境でデータベースを作成 */
+sudo mysql -e "CREATE DATABASE recipe"
+
+/* インポート実施 (rootユーザーの認証方式は「auth_socket」となっているため、OSユーザーのrootとして接続) */
+sudo mysql recipe < /tmp/recipe.sql
+
+/* DBユーザー作成 */
+CREATE USER 'recipe_user'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+/* 権限を付与（必要に応じて調整） */
+GRANT SELECT, INSERT, UPDATE, DELETE ON recipe.* TO 'recipe_user'@'%';
